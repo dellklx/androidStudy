f9dc1c21551ac78d901c874c235bfdac5a97316f
dellklx <dellklx@163.com>
2019-07-17 08:34:08 +0800

    添加第二单元

---
+++ GeoQuiz/app/src/main/java/android/bignerdranch/com/Question.java
@@ -0,0 +1,27 @@
+package android.bignerdranch.com;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResID, boolean answerTrue){
+        mTextResId = textResID;
+        mAnswerTrue = answerTrue;
+    }
+}
